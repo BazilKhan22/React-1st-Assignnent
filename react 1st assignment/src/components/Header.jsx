cd9e416341760c4587c9307212072226036344a3
BAZIL UR REHMAN <bazilurrehman08@gmail.com>
2025-04-30 15:55:29 +0500

    Add files via upload

---
+++ react 1st assignment/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <header style={{ background: "#333", color: "white", padding: "10px" }}>
+        <h1>Welcome to Bazil's Website</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
---
+++ react 1st assignment/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+    return (
+      <section style={{ padding: "20px", background: "#f0f0f0" }}>
+        <h2>This is the Main section of my Website</h2>
+        <p>This section highlights the main content of the page.</p>
+      </section>
+    );
+  }
+  
+  export default Hero;
+  
